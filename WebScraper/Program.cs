0b5fb9522a49e50b100f697a5ca7860ed77ccab2
Osman <osmanyalcinkaya@gmail.com>
2022-04-07 01:41:29 +0300

    selenium scraper demo

---
+++ WebScraper/Program.cs
@@ -0,0 +1,26 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+//get driver instance
+IWebDriver driver = new ChromeDriver();
+
+//set scraping url
+driver.Url = "https://toptalent.co/turkiyedeki-universiteler-listesi";
+
+//get table element
+IWebElement table = driver.FindElement(By.XPath("//*[@id='maincontainer']/div/div/div[4]/div/div/div[1]/table"));
+
+//select all rows
+IList<IWebElement> rows = table.FindElements(By.TagName("tr"));
+
+//foreach rows
+foreach (var item in rows)
+{
+    //select all columns
+    IList<IWebElement> columns = item.FindElements(By.TagName("td"));
+
+    //foreach columns
+    Console.WriteLine(columns[2].Text);
+    Console.WriteLine(columns[3].Text);
+    Console.WriteLine(columns[4].Text);
+}
